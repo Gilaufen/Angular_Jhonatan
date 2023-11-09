f3ae05f01bc22b40b2c082e20dc61df2380d9034
juanDsalinas <123585740+juanDsalinas@users.noreply.github.com>
2023-11-09 13:07:07 -0500

    Primeros Componentes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponentComponent } from './dashboard-component/dashboard-component.component';
+import { RegistroComponent } from './registro/registro.component';
+
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'dashboard', component: DashboardComponentComponent},
+  {path: 'registro', component: RegistroComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
