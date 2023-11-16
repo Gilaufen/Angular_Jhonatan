1ad5ed65f5c127cabefc179cb6e02608babbae0e
Gilaufen <131225939+Gilaufen@users.noreply.github.com>
2023-11-15 22:55:24 -0500

    FINALIZED: method =(recuperarContrasena())

--- src/app/recuperacion/recuperacion.component.ts
+++ src/app/recuperacion/recuperacion.component.ts
@@ -11,53 +11,53 @@ export class RecuperacionComponent {
 
   formulario: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router){
+  constructor(private fb: FormBuilder, private router: Router) {
     this.formulario = this.fb.group({
       nombre: ['', [Validators.required]],
       contraseña: ['', [Validators.required]]
     });
+    this.recuperarContrasena();
   }
 
-  recuperarContrasena(){
+  recuperarContrasena() {
     const nombreRecogido = this.formulario.get('nombre');
-    const nuevaContraseña = this.formulario.get('constraseña');
+    const nuevaContraseña = this.formulario.get('contraseña');
     const u = localStorage.getItem('personas');
 
-)
 
-  console.log("usuarios:", u);
-  console.log("nombreRecogido:", nombreRecogido);
-  console.log("nuevaContraseña:", nuevaContraseña);
 
-    if(u != null && nombreRecogido && nuevaContraseña){
-
+    if (u != null && nombreRecogido && nuevaContraseña) {
       console.log("entro 1");
-      
 
       const nombre = nombreRecogido.value;
       const contraseña = nuevaContraseña.value;
 
-      
-
       const usu = JSON.parse(u);
 
-      if(nombre !== '' && contraseña !==''){
+      console.log("usuarios:", usu);
+      console.log("nombreRecogido:", nombre);
+      console.log("nuevaContraseña:", contraseña);
 
-      for(let i = 0; i < usu.length; i++){
-        console.log("entro 2");
-        
-        if(usu[i].nombre === nombre){
-          // Cambiar la contraseña del usuario
-          usu[i].contraseña = contraseña;
-          // Actualizar el localStorage
-          localStorage.setItem("personas", JSON.stringify(usu));
 
-          alert("Contraseña cambiada");
-          return; // Terminar el bucle después de encontrar el usuario
+      if (nombre !== '' && contraseña !== '') {
+        for (const element of usu) {
+          console.log("entro 2");
+          console.log(element.nombre);
+
+          if (element.nombre.toLowerCase() === nombre.toLowerCase()) {
+
+            console.log("entro 3")
+            nuevaContraseña.setValue('');
+
+            element.contraseña = contraseña;
+            localStorage.setItem("personas", JSON.stringify(usu));
+
+            alert("Contraseña cambiada");
+            return;
+          }
         }
       }
     }
   }
 
-  }
 }
